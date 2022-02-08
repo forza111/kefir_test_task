cacb8dbe85dec6cec1c1a2657c29ca8e225a2849
forza111 <nikitos.ionkin@yandex.ru>
2022-02-08 09:30:13 +0300

    add route get_current_user

--- routers.py
+++ routers.py
@@ -18,14 +18,21 @@ app = APIRouter(tags=["notes"])
 @app.post("/login", response_model=schemas.CurrentUserResponseModel, responses={
     400: {"model": schemas.ErrorResponseModel, "description": "Bad Request"},
 })
-async def login(body_user: schemas.LoginModel,db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.login == body_user.login).first()
+async def login(request: Request,body_user: schemas.LoginModel,db: Session = Depends(get_db)):
+    user = Authenticate.get_user_by_login(db,body_user.login)
     if user is None or not Authenticate.verify_password(body_user.password, user.password):
         return JSONResponse(status_code=400, content={"code": 400, "message": "Wrong login or password"})
     if Authenticate.verify_password(body_user.password, user.password):
+        data = {"sub": body_user.login}
+        Authenticate.create_access_token(data,request)
         user_detail = user.user_detail
         return {"first_name": user_detail.first_name, "last_name": user_detail.last_name,
                 "other_name": user_detail.other_name, "email": user_detail.email,
                 "phone": user_detail.phone, "birthday": user_detail.birthday,
                 "is_admin": user_detail.is_admin
                 }
+
+@app.get("/users/current")
+async def get_current_user(current_user: schemas.LoginModel = Depends(Authenticate.get_current_user),
+                           db: Session = Depends(get_db)):
+    return {"current_user":current_user}
