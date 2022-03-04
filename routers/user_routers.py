b1222c7d0e27f685f2f69f8fe29795014daa8efc
forza111 <nikitos.ionkin@yandex.ru>
2022-03-04 09:18:08 +0300

    change router patch(users/pk) user can no longer transfer pk, it is transferred automatically

--- routers/user_routers.py
+++ routers/user_routers.py
@@ -14,8 +14,7 @@ router = APIRouter(tags=["user"])
 
 @router.get("/users/current", response_model=schemas.CurrentUserResponseModel, responses=responses.get_users_current)
 async def get_current_user(
-        current_user: schemas.LoginModel = Depends(Authenticate.get_current_user),
-        db: Session = Depends(get_db)
+        current_user: schemas.LoginModel = Depends(Authenticate.get_current_user)
         ):
     if current_user is None:
         return JSONResponse(status_code=401, content={"title": "Response 401 Current User Users Current Get"})
@@ -37,18 +36,12 @@ async def get_users(
 
 @router.patch("/users/{pk}", response_model=schemas.UpdateUserResponseModel, responses=responses.patch_users_pk)
 async def edit_user(
-        pk: int,
         update_user_body: schemas.UpdateUserModel,
         current_user: schemas.LoginModel = Depends(Authenticate.get_current_user),
         db: Session = Depends(get_db)
         ):
     if current_user is None:
         return JSONResponse(status_code=401, content={"title": "Response 401 Current User Users Current Get"})
-    request_user = crud.get_user_detail(db, pk)
-    if request_user is None:
-        return JSONResponse(status_code=404, content={"title": "Response 404 Edit User Users  Pk  Patch"})
-    if current_user.id != pk:
-        return JSONResponse(status_code=400, content={"code": 400,
-                                                      "message": "The user can only change their information"})
+    pk = current_user.id
     update_user = crud.update_db_user(db, pk, update_user_body)
     return update_user
